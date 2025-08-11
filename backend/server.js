077b1b3b3c73c3d67c41d6eb6a21062c9b3df509
Pratyush Tripathi <pratyushtripathi4592@gmail.com>
2025-08-11 23:57:44 +0530

    requirements.txt installation and changes

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,11 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const { spawn } = require('child_process');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
